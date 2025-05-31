56e9b02833164436e344b0b2f909d1ceb2821af3
尹志祥 <13602547976@163.com>
2025-06-01 02:05:36 +0800

    feat: finish_docs: VueChatPro version=0.0.0

---
+++ src/components/Welcome/index.ts
@@ -0,0 +1,9 @@
+import type { App } from "vue";
+import Welcome from "./Welcome.vue";
+
+Welcome.install = (app: App) => {
+  app.component("Welcome", Welcome);
+};
+
+export default Welcome;
+export * from "./types";
